3b9b021c805c3392b03eaeee1bc12ea16d5b7ef0
Alexandr Belotsitsko <sanyabelyy020@gmail.com>
2026-05-07 12:30:10 +0600

    fix подключения version.php

--- install/index.php
+++ install/index.php
@@ -16,7 +16,7 @@ class beeralex_user extends CModule
     public function __construct()
     {
         if (is_file(__DIR__ . '/version.php')) {
-            include_once(__DIR__ . '/version.php');
+            include __DIR__ . '/version.php';
             $this->MODULE_ID           = 'beeralex.user';
             $this->MODULE_VERSION      = $arModuleVersion['VERSION'];
             $this->MODULE_VERSION_DATE = $arModuleVersion['VERSION_DATE'];
